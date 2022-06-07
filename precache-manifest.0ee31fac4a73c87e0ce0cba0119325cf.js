ff56bfb56e4407555863342ce10513cd5146828b
Umair <razaumair860@gmail.com>
2022-06-07 09:05:17 +0500

    Updates

---
+++ precache-manifest.0ee31fac4a73c87e0ce0cba0119325cf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5dcff4814a6f58e6e7e08df927c41ab",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "33e1723fdaaca0ae0f83",
+    "url": "/Keeper-App/static/js/2.5e4e9caa.chunk.js"
+  },
+  {
+    "revision": "2fd2a920b79d86385f61",
+    "url": "/Keeper-App/static/js/main.21dc68dd.chunk.js"
+  },
+  {
+    "revision": "128fd620914684fbc76b",
+    "url": "/Keeper-App/static/js/runtime-main.113ea231.js"
+  }
+]);
